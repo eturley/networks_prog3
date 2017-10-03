71fa26f55b4ece9835e0943bdeeb55d0510b39d5
Mike Parowski <mparowsk@nd.edu>
2017-10-03 17:51:08 -0400

    set up basic tcp stuff

--- udpserver.c
+++ udpserver.c
@@ -9,8 +9,9 @@
 #include <netdb.h>
 #include <time.h>
 #include <unistd.h>
-#define SERVER_PORT 41100
+#define SERVER_PORT 41025
 #define MAX_LINE 4096
+#define MAX_PENDING 5
 #define MAX_KEY 16
 
 
@@ -24,7 +25,7 @@ int main(int argc, char * argv[]) {
 	int stampLength, i;
 	struct sockaddr_in sin, client_addr;
 	char buf[MAX_LINE];
-	int len, addr_len,s;
+	int len, addr_len,s, new_s;
 	int portNum; int messageLength;
 	char key[MAX_KEY];
 	char *fullMessage;
@@ -50,8 +51,14 @@ int main(int argc, char * argv[]) {
 	
 	
 	/* setup passive open*/
-	if ((s = socket(PF_INET, SOCK_DGRAM,0)) < 0) {
+	if ((s = socket(PF_INET, SOCK_STREAM,0)) < 0) {
 		perror("simplex-talk:socket");
+	}
+
+	/* set socket option*/
+	if((setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)& opt, sizeof(int))) < 0) {
+		perror("simplex-talk:setsockopt\n");
+		exit(1);
 	}
 	
 	if ((bind(s, (struct sockaddr *) &sin, sizeof(sin))) < 0) {
@@ -62,26 +69,49 @@ int main(int argc, char * argv[]) {
 	addr_len = sizeof(sin);
 	
 	while (1) {
-		/* receive initial message */
-		if (recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *) &sin, &addr_len)==-1) {
-			perror("Receive error!\n");
+		if((listen(s, MAX_PENDING)) < 0) {
+			perror("simplex-talk: bind\n");
+		}
+
+		if((new_s = accept(s, (struct sockaddr *)&sin, &addr_len)) < 0) {
+			perror("simplex-talk: accept\n");
 			exit(1);
 		}
-		
-		
+
+		if((len = recv(new_s, buf, sizeof(buf), 0)) == -1) {
+			perror("Server Received Error!\n");
+			exit(1);
+		}
+		if(len == 0) break;
+		printf("TCP Server Received: %s\n", buf);
 		bzero((char*)&buf,sizeof(buf));
-		
-		
-		//send encryption key
-		if (sendto(s, encKey, strlen(encKey) + 1, 0, (struct sockaddr *)&sin, sizeof(struct sockaddr)) == -1) {
-		    perror ("Error sending from client!\n");
-		    exit(1);
+
+		if(strcmp(buf, "DWNLD") == 0) {
+			// do download stuff
 		}
-		
-		/* receive message to encrypt*/
-		if (messageLength = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *) &sin, &addr_len)==-1) {
-			perror("Receive error!\n");
-			exit(1);
+		else if(strcmp(buf, "UPLD") == 0) {
+			// upload
+		}
+		else if(strcmp(buf, "DELF") == 0) {
+			// delete file
+		}
+		else if(strcmp(buf, "LIST") == 0) {
+			// ls
+		}
+		else if(strcmp(buf, "MDIR") == 0) {
+			// mkdir
+		}
+		else if(strcmp(buf, "RDIR") == 0) {
+			// rmdir
+		}
+		else if(strcmp(buf, "CDIR") == 0) {
+			// cd
+		}
+		else if(strcmp(buf, "QUIT") == 0) {
+			// quit
+		}
+		else {
+			printf("Invalid Command\n");
 		}
 		
 		/*create time stamp*/
@@ -90,27 +120,7 @@ int main(int argc, char * argv[]) {
 		printf("%d:%d:%d\n", Tm->tm_hour, Tm->tm_min, Tm->tm_sec);
 		gettimeofday(&tv, NULL);
 		sprintf(timeStamp, "%d:%d:%d.%d", Tm->tm_hour, Tm->tm_min, Tm->tm_sec, tv.tv_usec);
-		
-		
-		/* create message to encrypt */
-		messageLength = sprintf(encMessage, "%s Timestamp: %s", buf, timeStamp);
-		
-		
-		/* encrypt message */
-		for (i=0; i < messageLength; i+=1) {
-			encMessage[i] = encMessage[i]^encKey[i%(strlen(encKey)+1)];
-		}
-		
-		/* send encrypted message back to client */
-		if (sendto(s, encMessage, messageLength, 0, (struct sockaddr *)&sin, sizeof(struct sockaddr)) == -1) {
-		    perror ("Error sending from client!\n");
-		    exit(1);
-		}
-		
-		
 		bzero((char*)&buf,sizeof(buf));
-		
-		
 	}
 	if (close(s) != 0) {
         perror("Was not closed!\n");
